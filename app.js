3d591ed6d198f9ead18fbfd8dab42856b88d91fd
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-04-29 17:23:26 -0300

    feat: adding handlebar views

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const db = require("./db/connection");
+const exphbs = require("express-handlebars");
 const app = express();
+const path = require("path");
+const db = require("./db/connection");
 const bodyParser = require("body-parser");
 
 const PORT = 3000;
@@ -11,6 +13,14 @@ app.listen(PORT, function () {
 
 //body parser
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//handlebars
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 //db connection
 db.authenticate()
@@ -23,7 +33,7 @@ db.authenticate()
 
 //routes
 app.get("/", (req, res) => {
-  res.send("Está funcionando");
+  res.render("index");
 });
 
 //jobs routes
--- routes/jobs.js
+++ routes/jobs.js
@@ -6,6 +6,10 @@ router.get("/test", (req, res) => {
   res.send("Deu Certo amigo");
 });
 
+router.get("/add", (req, res) => {
+  res.render("add");
+});
+
 // add job via post
 router.post("/add", (req, res) => {
   let { TITLE, SALARY, COMPANY, DESCRIPTION, EMAIL, NEW_JOB } = req.body;
