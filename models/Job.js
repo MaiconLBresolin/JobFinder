e973b788d7c0d7a8af431b0a05e0fb06589f8e8b
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-04-29 13:24:26 -0300

    feat: adding models

---
+++ models/Job.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize");
+
+const db = require("../db/connection");
+
+const Job = db.define("job", {
+  TITLE: {
+    type: Sequelize.STRING,
+  },
+  DESCRIPTION: {
+    type: Sequelize.STRING,
+  },
+  SALARY: {
+    type: Sequelize.STRING,
+  },
+  COMPANY: {
+    type: Sequelize.STRING,
+  },
+  EMAIL: {
+    type: Sequelize.STRING,
+  },
+  NEW: {
+    type: Sequelize.INTEGER,
+  },
+});
+
+module.exports = Job;
